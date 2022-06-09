ac56aee985726ea76d846299c47596761a6854ee
ediah <60201514+ediah@users.noreply.github.com>
2022-06-09 20:12:56 +0300

    Задокументировано объявление Gendarme

--- src/common/stack.hpp
+++ src/common/stack.hpp
@@ -21,6 +21,7 @@ class Gendarme {
     int pos;                   //!< Текущая позиция в массиве.
 
 public:
+    //! Конструктор
     Gendarme(void): pos(0) {
         for (int i = 0; i < MAXSTACK; i++) {
             pointers[i] = nullptr;
@@ -28,12 +29,27 @@ public:
         }
     };
 
+    /** Добавить новый указатель в массив.
+     *  @param[in] p    Указатель на выделенную память
+     *  @param[in] type Тип хранящегося в ней значения
+     */
     void push(void * p, type_t type);
+
+    //! Освободить память по всем сохранённым указателям.
     void burn(void);
+
+    //! Узнать тип последнего элемента.
     type_t topType(void) const;
+
+    //! Получить массив типов.
     const type_t * getTypes(void) const;
+
+    /** Изменить тип последнего элемента.
+     *  @param[in] type Новый тип элемента
+     */
     void updateType(type_t type);
 
+    //! Деструктор
     ~Gendarme();
 };
 
